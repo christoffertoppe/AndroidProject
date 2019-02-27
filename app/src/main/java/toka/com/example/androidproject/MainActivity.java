2581b7847e470105f9a42bbf73f0bf73b0a1976f
SamSalin <SalinSamuli@gmail.com>
2019-02-27 19:32:17 +0200

    Custom font added to assets/fonts Example how to use the font added to MainActivity

--- app/src/main/java/toka/com/example/androidproject/MainActivity.java
+++ app/src/main/java/toka/com/example/androidproject/MainActivity.java
@@ -1,7 +1,9 @@
 package toka.com.example.androidproject;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Esimerkki fontin vaihdosta
+        TextView tx = findViewById(R.id.exampleView);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/colophon.ttf");
+        tx.setTypeface(typeface);
+
     }
 }
