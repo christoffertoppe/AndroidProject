5fe4cc647b24e593bc69ff5b598667d8460bb14b
christoffertoppe <chris.mailbox85@gmail.com>
2019-02-28 12:07:37 +0200

    update own branch

--- app/src/main/java/toka/com/example/androidproject/MainActivity.java
+++ app/src/main/java/toka/com/example/androidproject/MainActivity.java
@@ -1,13 +1,31 @@
 package toka.com.example.androidproject;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MAIN MENU";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button next = findViewById(R.id.button);
+        next.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Log.d(TAG, "onItemClick");
+                Intent myIntent = new Intent(view.getContext(), UserProfile.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
+
---
+++ app/src/main/java/toka/com/example/androidproject/UsefulTips.java
@@ -0,0 +1,37 @@
+package toka.com.example.androidproject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsefulTips {
+
+    private List<String> tips;
+
+    private static final UsefulTips ourInstance = new UsefulTips();
+
+    static UsefulTips getInstance() {
+        return ourInstance;
+    }
+
+    private UsefulTips() {
+        tips = new ArrayList<>();
+        tips.add("Muista käyttää flourihammastahnaa.");
+        tips.add("Muista pestä takahampaat.");
+        tips.add("Harjaa hellästi hampaitasi.");
+    }
+
+    public String getTip(int i) {
+        return tips.get(i);
+    }
+
+    public int getListSize() {
+        return tips.size();
+    }
+
+    public void addTip(String tip) {
+        tips.add(tip);
+    }
+    public List<String> getTips() {
+        return tips;
+    }
+}
